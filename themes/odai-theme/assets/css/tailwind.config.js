4cacc85ee2521c0500c13b7158a7a4bcfdba6342
odhyp <pradhana.odhy@gmail.com>
2024-08-24 19:23:49 +0700

    feat: add flowbite js

--- themes/odai-theme/assets/css/tailwind.config.js
+++ themes/odai-theme/assets/css/tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "./themes/**/layouts/**/*.html",
     "./content/**/layouts/**/*.html",
     "./layouts/**/*.html",
-    "./content/**/*.html"
+    "./content/**/*.html",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
